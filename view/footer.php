b2a8647c3d970429a47edae301e25df2fb1472b1
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-04-23 10:47:09 +0530

    slider

--- view/footer.php
+++ view/footer.php
@@ -1,3 +1,3 @@
 <div class="conatiner-fluid bg-dark text-light">
-    <p class="text-center md-0">Copyright Atul Pratap Singh 2022 | All Rights reserved</p>
+    <p class="text-center mb-0">Copyright Atul Pratap Singh 2022 | All Rights reserved</p>
 </div>
